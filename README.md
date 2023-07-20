047d5331408a9bdebecfa31bfdebb1f62032deda
kostix <kostix@macPro19.local>
2023-07-20 21:42:55 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,141 @@
+# Intervaq
+> Just another one solution for `intervals` \ `timeouts` via `requestAnimationFrame`.
+
+Working well in a project that based on [three.js][threejs-url] library.
+
+
+
+## Why:
+  - to use `intervals` \ `timeouts` via [requestAnimationFrame][requestAnimationFrame-url];
+  - to avoid some time based glitches, violations;
+  - to control some time based actions.
+
+
+
+## Some info to use:
+  - `intervaq`:
+    - `.checkToExecute()` in `requestAnimationFrame` callback body;
+    - `.setInterval(callback, timeMs)` / `.clearInterval(interval)` as usual;
+    - `.setTimeout(callback, timeMs)` / `.clearTimeout(timeout)` as usual;
+    - `.pauseProcessing()` / `.continueProcessing()` when its necessary;
+  - `intervaq` . `intervals` / `timeouts`:
+    - `.pauseExecuting(currentTimeInt)` and `.continueExecuting(currentTimeInt)` manually;
+    - `.disable()` / `.enable()` / `.restart()` manually.
+
+
+
+## Usage:
+
+sode sample:
+
+```javascript
+
+import { Intervaq } from 'intervaq';
+
+
+
+// init intervaq object
+const intervaq = new Intervaq();
+
+// using intervaq via requestAnimationFrame
+function animate() {
+  requestAnimationFrame( animate );
+
+  // here
+  if (intervaq !== undefined)
+    intervaq.checkToExecute();
+}
+
+
+
+// to control visibility state
+document.addEventListener( 'visibilitychange', onVisibilityChange );
+
+function onVisibilityChange ( event ) {
+  if (event.target.visibilityState === 'visible') {
+    console.log(`tab is active at ${new Date().getTime()} `);
+    // continue processing
+    intervaq.continueProcessing();
+  } else {
+    console.log(`tab is inactive at ${new Date().getTime()} `);
+    // pause processing
+    intervaq.pauseProcessing();
+  }
+}
+
+
+
+// intervaq.setInterval case:
+let testIntervalValue = 0;
+const testInterval = intervaq.setInterval( () => {
+  console.log(`testInterval every 1000ms #${testIntervalValue} at ${new Date().getTime()} `);
+  testValue++;
+}, 1000);
+
+
+
+// intervaq.setTimeout case:
+let testTimeoutValue = 0;
+const testTimeout = intervaq.setTimeout( () => {
+  // disable its testInterval
+  intervaq.clearInterval(testInterval);
+  console.log(`testTimeout in 5500ms #${testTimeoutValue} at ${new Date().getTime()} `);
+}, 5500);
+
+
+
+// action
+animate();
+
+```
+
+output sample 0:
+```
+testInterval every 1000ms #0 at 1689861750168 
+testInterval every 1000ms #1 at 1689861751169 
+testInterval every 1000ms #2 at 1689861752184 
+testInterval every 1000ms #3 at 1689861753184 
+testInterval every 1000ms #4 at 1689861754201 
+testTimeout in 5500ms #0 at 1689861754667
+```
+
+output sample 1 (on tabs switching):
+```
+testInterval every 1000ms #0 at 1689877224270 
+testInterval every 1000ms #1 at 1689877225287 
+tab is inactive at 1689877226100 
+tab is active at 1689877230127 
+testInterval every 1000ms #2 at 1689877230319 
+tab is inactive at 1689877231240 
+tab is active at 1689877234740 
+testInterval every 1000ms #3 at 1689877234820 
+testInterval every 1000ms #4 at 1689877235821 
+testTimeout in 5500ms #0 at 1689877236288
+
+```
+
+
+
+## TODO:
+
+  - [ ] apply some pattern... maybe...
+  - [ ] mk some prettify
+  - [ ] mk some doc
+  - [ ] mk some lint
+  - [ ] mk some tests
+  - [ ] mk some cleanup... maybe...
+  - [ ] modify some `checkToExecute` functionality
+  - [ ] try to keep pausing at its `Intervaq` class only
+  - [ ] do smth with `destroy` method
+  - [ ] transfer to typescript... maybe...
+
+
+
+---
+© [kostix.dev][kostix-url]
+
+
+
+[kostix-url]: https://kostix.dev
+[threejs-url]: https://threejs.org
+[requestAnimationFrame-url]: https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
---
+++ src/index.js
@@ -0,0 +1,252 @@
+
+function getCurrentTime() {
+  return (performance || Date).now();
+}
+
+
+
+const STATUS_INTERVAQ_PAUSED = 0;
+const STATUS_INTERVAQ_IN_PROCESS = 1;
+
+class Intervaq {
+  intervals = [];
+  timeouts = [];
+
+  status = STATUS_INTERVAQ_IN_PROCESS;
+  pausedAt = null;
+
+  constructor() {
+    // nothing to do here
+  }
+
+  setInterval(fnToExecute, timeInterval) {
+    const interval = new Interval(fnToExecute, timeInterval);
+    this.intervals.push(interval);
+    return interval;
+  }
+
+  // @TODO: chck clearInterval on executionInProcess
+  // @TODO: test for memory
+
+  clearInterval(interval) {
+    const index = this.intervals.indexOf(interval);
+    if (index !== -1) {
+      interval.destroy();
+      this.intervals.splice(index, 1);
+      return true;
+    }
+    return false;
+  }
+
+  setTimeout(fnToExecute, timeOut) {
+    const timeout = new Timeout(fnToExecute, timeOut);
+    this.timeouts.push(timeout);
+    return timeout;
+  }
+
+  clearTimeout(timeout) {
+    const index = this.timeouts.indexOf(timeout);
+    if (index !== -1) {
+      timeout.destroy();
+      this.timeouts.splice(index, 1);
+      return true;
+    }
+    return false;
+  }
+
+  checkToExecute() {
+    if (this.status !== STATUS_INTERVAQ_IN_PROCESS) {
+      return;
+    }
+    const timeCurrent = getCurrentTime();
+    // - intervals execute
+    this.intervals.forEach((interval) =>
+      interval.checkTimeToExecute(timeCurrent)
+    );
+    // - timeouts execute
+    this.timeouts.forEach((timeout, index) => {
+      if (timeout.checkTimeToExecute(timeCurrent)) {
+        timeout.destroy();
+        this.timeouts.splice(index, 1);
+      }
+    });
+  }
+
+  pauseProcessing() {
+    this.status = STATUS_INTERVAQ_PAUSED;
+    this.pausedAt = getCurrentTime();
+    // - intervals pause
+    this.intervals.forEach( interval => interval.pauseExecuting(this.pausedAt) );
+    // - timeouts pause
+    this.timeouts.forEach( timeout => timeout.pauseExecuting(this.pausedAt) );
+  }
+
+  continueProcessing() {
+    this.status = STATUS_INTERVAQ_IN_PROCESS;
+    this.pausedAt = null;
+    const runAt = getCurrentTime();
+    // - intervals run
+    this.intervals.forEach( interval => interval.continueExecuting(runAt) );
+    // - timeouts run
+    this.timeouts.forEach( timeout => timeout.continueExecuting(runAt) );
+  }
+}
+
+
+
+const STATUS_INTERVAL_PAUSED = 0;
+const STATUS_INTERVAL_IN_PROCESS = 1;
+const STATUS_INTERVAL_DISABLED = 3;
+
+class Interval {
+  prevTime = null;
+  callback = () => null;
+  timeInterval = null;
+
+  executeAtTime = null;
+
+  status = STATUS_INTERVAL_IN_PROCESS;
+  pausedAtTime = null;
+
+  constructor(callback, timeInterval) {
+    this.prevTime = getCurrentTime();
+    this.callback = callback;
+    this.timeInterval = timeInterval;
+    this.executeAtTime = this.prevTime + timeInterval;
+  }
+
+  checkTimeToExecute(timeToCheck) {
+    if (this.status !== STATUS_INTERVAL_IN_PROCESS) {
+      return;
+    }
+    if (timeToCheck >= this.executeAtTime) {
+      this.execute();
+      this.prevTime = timeToCheck;
+      this.executeAtTime = timeToCheck + this.timeInterval;
+    }
+  }
+
+  execute() {
+    this.callback.call();
+  }
+
+  pauseExecuting(pausedAtTime) {
+    this.status = STATUS_INTERVAL_PAUSED;
+    this.pausedAtTime = pausedAtTime;
+  }
+
+  continueExecuting(continueAtTime) {
+    this.executeAtTime += continueAtTime - this.pausedAtTime;
+    this.pausedAtTime = null;
+    this.status = STATUS_INTERVAL_IN_PROCESS;
+  }
+
+  disable() {
+    this.status = STATUS_INTERVAL_DISABLED;
+    return this;
+  }
+
+  enable() {
+    this.prevTime = getCurrentTime();
+    this.executeAtTime = this.prevTime + this.timeOut;
+    this.status = STATUS_INTERVAL_IN_PROCESS;
+    return this;
+  }
+
+  restart() {
+    this.disable();
+    this.enable();
+    return this;
+  }
+
+  destroy() {
+    this.prevTime = null;
+    this.callback = () => null;
+    this.timeInterval = null;
+    this.status = null;
+    this.pausedAtTime = null;
+  }
+}
+
+
+
+const STATUS_TIMEOUT_PAUSED = 0;
+const STATUS_TIMEOUT_IN_PROCESS = 1;
+const STATUS_TIMEOUT_DONE = 2;
+const STATUS_TIMEOUT_DISABLED = 3;
+
+class Timeout {
+  prevTime = null;
+  callback = () => null;
+  timeOut = null;
+
+  executeAtTime = null;
+
+  status = STATUS_TIMEOUT_IN_PROCESS;
+  pausedAtTime = null;
+
+  constructor(callback, timeOut) {
+    this.prevTime = getCurrentTime();
+    this.callback = callback;
+    this.timeOut = timeOut;
+    this.executeAtTime = this.prevTime + this.timeOut;
+  }
+
+  checkTimeToExecute(timeToCheck) {
+    if (
+      timeToCheck >= this.executeAtTime &&
+      this.status === STATUS_TIMEOUT_IN_PROCESS
+    ) {
+      return this.execute();
+    }
+    return false;
+  }
+
+  execute() {
+    this.callback.call();
+    this.status = STATUS_TIMEOUT_DONE;
+    return true;
+  }
+
+  pauseExecuting(pausedAtTime) {
+    this.status = STATUS_TIMEOUT_PAUSED;
+    this.pausedAtTime = pausedAtTime;
+  }
+
+  continueExecuting(continueAtTime) {
+    this.executeAtTime += continueAtTime - this.pausedAtTime;
+    this.pausedAtTime = null;
+    this.status = STATUS_TIMEOUT_IN_PROCESS;
+  }
+
+  disable() {
+    this.status = STATUS_TIMEOUT_DISABLED;
+    return this;
+  }
+
+  enable() {
+    this.prevTime = getCurrentTime();
+    this.executeAtTime = this.prevTime + this.timeOut;
+    this.status = STATUS_TIMEOUT_IN_PROCESS;
+    return this;
+  }
+
+  restart() {
+    this.disable();
+    this.enable();
+    return this;
+  }
+
+  destroy() {
+    this.prevTime = null;
+    this.callback = () => null;
+    this.timeOut = null;
+    this.executeAtTime = null;
+    this.status = null;
+    this.pausedAtTime = null;
+  }
+}
+
+
+
+export { Intervaq, Interval, Timeout };
